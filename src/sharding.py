d91b4b72f5c5a414c45e65baa6d9f4e8333f5d8c
Max <bolgov1458@yandex.ru>
2024-03-14 13:42:33 +0300

    Update sharding.py

--- src/sharding.py
+++ src/sharding.py
@@ -47,9 +47,9 @@ sharding_mp = Phi(
             attention=Attention(q_proj=Proj(weight=(1, 3), bias=...), k_proj==Proj(weight=(1, 2), bias=...), v_proj==Proj(weight=(1, 2), bias=...), dense==Proj(weight=(2, 4), bias=...),
             gate_proj=Proj(weight=(1, 2), bias=...),
             up_proj=Proj(weight=(1, 2), bias=...),
-            down_proj==Proj(weight=(2, 1), bias=...)
+            down_proj==Proj(weight=(2, 1), bias=...),
         ),
-        final_layernorm=Layernorm(weight=..., bias=...)
+        final_layernorm=Layernorm(weight=..., bias=...),
     ),
     lm_head=Proj(weight=..., bias=...),
 )
